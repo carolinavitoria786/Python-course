3516d0dcf57daab7766b7d4de0f4a2865b95a808
Carolina Vitoria Oliveira <carolinavitoria786@gmail.com>
2025-07-14 15:41:38 -0300

    att

---
+++ aulas/while_break.py
@@ -0,0 +1,52 @@
+"""
+Repetições 
+while(enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 0 
+
+while contador <= 10: 
+    contador = contador + 1
+    print(contador)
+    
+print('Acabou')
+
+#-----Ex.: while, continue e break----------------#
+
+contador = 0 
+
+while contador <= 100:
+    contador += 1
+    
+    if contador == 6:
+        print('Não vou mostrar o 6.')
+        continue
+    
+    if contador >= 10 and contador <= 27:
+        print('Não vou mostrar o', contador)
+        continue
+    
+    print(contador)
+    if contador == 40:
+        break 
+
+    print('Acabou')
+    
+#------------while dentro de while----------------#
+
+qtd_linha = 5
+qtd_coluna = 5
+
+linha = 1 
+while linha <= qtd_coluna:
+    coluna = 1
+    while coluna <= qtd_coluna:
+        print(f'{linha=}{coluna=}')
+        coluna += 1
+    linha += 1
+    
+print('Acabou')
---
+++ exercicios/aula64.py
@@ -0,0 +1,23 @@
+"""
+Iteração de String com while
+"""
+
+nome = input('Digite seu nome: ')
+
+while nome :
+    tamanho = len(nome)
+    print(tamanho)
+    print(f'{nome}')
+    break
+
+#---------resolução do exercicio---------# 
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}' #*C*a*r*o*l*i*n*a* *V*i*t*ó*r*i*a
+    indice += 1
+
+novo_nome += '*'  #acrescenta o asteristico que falta no final da string  
+print(novo_nome)
