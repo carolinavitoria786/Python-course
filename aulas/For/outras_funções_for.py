b452e954328760dcd0d9cea596b188c705d78e4b
Carolina Vitoria Oliveira <carolinavitoria786@gmail.com>
2025-07-26 12:10:34 -0300

    att

--- aulas/For/outras_funções_for.py
+++ aulas/For/outras_funções_for.py
@@ -11,7 +11,7 @@ for i in range(10):                                 #informando apenas o fim nes
         print('i é 8, seu else não executará')
         break                                       #break encerra o codigo 
     
-    for i in range(1,3):                            #neste caso tendo "i" como linhas e "j" como colunas o range percorre de 1 a 2
+    for j in range(1,3):                            #neste caso tendo "i" como linhas e "j" como colunas o range percorre de 1 a 2
         print(i, j)
 else:                                               #o bloco for sendo completo o else será executado
     print('For completo com sucesso!')               
