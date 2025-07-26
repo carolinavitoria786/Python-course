feb35064ca732763decf2d38bd0493b243794d5a
Carolina Vitoria Oliveira <carolinavitoria786@gmail.com>
2025-07-26 11:41:17 -0300

    att

--- aulas/For/for.py
+++ aulas/For/for.py
@@ -9,3 +9,18 @@ numeros = range(0, 100, 8)
 
 for numero in numeros:
     print(numero)
+    
+#-------Explicação-de-qual-a-lógica-do-for-------#
+
+#for letra in texto:
+#    print(letra)
+
+texto = 'Carol' #iterável que possui os elementos
+iterador = iter(texto) #iterator 
+
+while True:
+    try:
+        letra = next(iterador) #retorna o proximo valor da variável que possui os elementos
+        print(letra)
+    except StopIteration:
+        break
---
+++ aulas/For/outras_funções_for.py
@@ -0,0 +1,17 @@
+"""
+O que foi aprendido com while também funciona no for (continue, break, else, etc)
+"""
+
+for i in range(10):                                 #informando apenas o fim neste range
+    if i == 2:
+        print('i é 2, pulando...')
+        continue                                    #continue faz com que retorne ao começo do laço
+    
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break                                       #break encerra o codigo 
+    
+    for i in range(1,3):                            #neste caso tendo "i" como linhas e "j" como colunas o range percorre de 1 a 2
+        print(i, j)
+else:                                               #o bloco for sendo completo o else será executado
+    print('For completo com sucesso!')               
